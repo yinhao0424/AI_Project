a76f5afe54fde14079da5bbdaf8d88671161fcda
Hao <44931709+yinhao0424@users.noreply.github.com>
2020-02-14 22:05:09 -0500

    NQueens.py

--- NQueens.py
+++ NQueens.py
@@ -1,6 +1,7 @@
 import numpy as np
 import copy
 
+
 class Queen():
 
     def input_board(self, filename):
@@ -34,20 +35,21 @@ class Queen():
         board = np.nonzero(state.T)[1]
         min_attact = []
         for i in range(len(board)):
-            for j in range(i+1,len(board)):
+            for j in range(i + 1, len(board)):
                 # check same rows attack
                 if board[i] == board[j]:
-                    min_attact.append(min(state[board[i],i],state[board[j],j]))
+                    min_attact.append(min(state[board[i], i], state[board[j], j]))
                 # check diagonally attack
-                offset = j-i
+                offset = j - i
                 if board[i] == board[j] - offset or board[i] == board[j] + offset:
-                    min_attact.append(min(state[board[i],i],state[board[j],j]))
+                    min_attact.append(min(state[board[i], i], state[board[j], j]))
+        if min_attact == []:
+            return 0
+        else:
+            h1 = min(min_attact) ** 2
+            return h1
 
-        h1 = min(min_attact)**2
-
-        return h1
-
-    def heuristic_2(self,state):
+    def heuristic_2(self, state):
         """
         H2: Sum across every pair of attacking Queens the weight of the lightest Queen.
         ex: min(3,9)**2 + min(9,1)**2 + min(3,1)**2 + min(4,2)**2 = 3**2 +1**2 +1**2 +2**2  = 15
@@ -57,19 +59,22 @@ class Queen():
         board = np.nonzero(state.T)[1]
         min_attact = []
         for i in range(len(board)):
-            for j in range(i+1,len(board)):
+            for j in range(i + 1, len(board)):
                 # check same rows attack
                 if board[i] == board[j]:
-                    min_attact.append(min(state[board[i],i],state[board[j],j]))
+                    min_attact.append(min(state[board[i], i], state[board[j], j]))
                 # check diagonally attack
-                offset = j-i
+                offset = j - i
                 if board[i] == board[j] - offset or board[i] == board[j] + offset:
-                    min_attact.append(min(state[board[i],i],state[board[j],j]))
+                    min_attact.append(min(state[board[i], i], state[board[j], j]))
 
-        h2 = sum([i**2 for i in min_attact])
-        return h2
+        if min_attact == []:
+            return 0
+        else:
+            h2 = sum([i ** 2 for i in min_attact])
+            return h2
 
-    def near_state(self,state):
+    def near_state(self, state):
         """
         input: state
         output: a list of all possible neighbours
@@ -81,8 +86,8 @@ class Queen():
             for row in range(len(board)):
                 if row != board[col]:
                     neighbour = copy.deepcopy(state)
-                    neighbour[row,col] = state[board[col],col]
-                    neighbour[board[col],col] = 0
+                    neighbour[row, col] = state[board[col], col]
+                    neighbour[board[col], col] = 0
 
                     near_states.append(neighbour)
 
