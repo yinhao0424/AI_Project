55b784830e775137e87b24cbc45b875796c576a2
Hao <44931709+yinhao0424@users.noreply.github.com>
2020-02-22 22:49:34 -0500

    Add files via upload

--- nq_astar.py
+++ nq_astar.py
@@ -9,7 +9,7 @@ def state_to_tuple(state):
     return tuple(np.nonzero(state.T)[1])
 
 
-def a_star(q, filename):
+def a_star(q, filename,Heuristic):
     state = q.input_board(filename)
     start = state_to_tuple(state)
 
@@ -22,28 +22,55 @@ def a_star(q, filename):
     cost_so_far[start] = 0
     result = None
     start = timeit.default_timer()
-    while frontier:
-        #         sorted(graded, key=lambda x: x[0])
-        frontier.sort(key=lambda x: x[0], reverse=True)
-        current = frontier.pop()
+    if Heuristic == 'H1':
+        while frontier:
+            #         sorted(graded, key=lambda x: x[0])
+            frontier.sort(key=lambda x: x[0], reverse=True)
+            current = frontier.pop()
 
-        if q.heuristic_1(current[-1]) == 0:
-            result = current[-1]
-            break
-        for next in q.near_state(current[-1]):
-            new_cost = cost_so_far[state_to_tuple(current[-1])] + q.cost(current[-1], next)
+            if q.heuristic_1(current[-1]) == 0:
+                result = current[-1]
+                break
+            for next in q.near_state(current[-1]):
+                new_cost = cost_so_far[state_to_tuple(current[-1])] + q.cost(current[-1], next)
 
-            if state_to_tuple(next) not in cost_so_far:
-                cost_so_far[state_to_tuple(next)] = new_cost
+                if state_to_tuple(next) not in cost_so_far:
+                    cost_so_far[state_to_tuple(next)] = new_cost
 
-                priority = new_cost + q.heuristic_1(next)
-                frontier.append((priority, next))
+                    priority = new_cost + q.heuristic_1(next)
+                    frontier.append((priority, next))
 
-                come_from[state_to_tuple(next)] = state_to_tuple(current[-1])
-        end = timeit.default_timer()
+                    come_from[state_to_tuple(next)] = state_to_tuple(current[-1])
 
-        if (end - start) > 10:
-            break
+            end = timeit.default_timer()
+
+            if (end - start) > 10:
+                break
+    if Heuristic == 'H2':
+        while frontier:
+            #         sorted(graded, key=lambda x: x[0])
+            frontier.sort(key=lambda x: x[0], reverse=True)
+            current = frontier.pop()
+
+            if q.heuristic_2(current[-1]) == 0:
+                result = current[-1]
+                break
+            for next in q.near_state(current[-1]):
+                new_cost = cost_so_far[state_to_tuple(current[-1])] + q.cost(current[-1], next)
+
+                if state_to_tuple(next) not in cost_so_far:
+                    cost_so_far[state_to_tuple(next)] = new_cost
+
+                    priority = new_cost + q.heuristic_2(next)
+                    frontier.append((priority, next))
+
+                    come_from[state_to_tuple(next)] = state_to_tuple(current[-1])
+
+            end = timeit.default_timer()
+
+            if (end - start) > 10:
+                break
+
 
     end = timeit.default_timer()
     during_time = end - start
@@ -53,7 +80,7 @@ def a_star(q, filename):
 if __name__ == "__main__":
     filename = 'heavy queens board.csv'
     q = Queen()
-    state, node_expanded, during_time, result = a_star(q, filename)
+    state, node_expanded, during_time, result = a_star(q, filename,'H1')
     print('start states: ')
     print(state)
     print('end state: ')
