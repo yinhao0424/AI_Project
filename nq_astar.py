5e955da974ed10ad146b583b9e8c4ee38aac5b96
Hao <44931709+yinhao0424@users.noreply.github.com>
2020-02-22 22:45:43 -0500

    Add files via upload

---
+++ nq_astar.py
@@ -0,0 +1,64 @@
+from NQueens import Queen
+import timeit
+import random
+import queue
+import numpy as np
+
+
+def state_to_tuple(state):
+    return tuple(np.nonzero(state.T)[1])
+
+
+def a_star(q, filename):
+    state = q.input_board(filename)
+    start = state_to_tuple(state)
+
+    frontier = []
+    frontier.append((0, state))
+
+    come_from = {}
+    cost_so_far = {}
+    come_from[start] = None
+    cost_so_far[start] = 0
+    result = None
+    start = timeit.default_timer()
+    while frontier:
+        #         sorted(graded, key=lambda x: x[0])
+        frontier.sort(key=lambda x: x[0], reverse=True)
+        current = frontier.pop()
+
+        if q.heuristic_1(current[-1]) == 0:
+            result = current[-1]
+            break
+        for next in q.near_state(current[-1]):
+            new_cost = cost_so_far[state_to_tuple(current[-1])] + q.cost(current[-1], next)
+
+            if state_to_tuple(next) not in cost_so_far:
+                cost_so_far[state_to_tuple(next)] = new_cost
+
+                priority = new_cost + q.heuristic_1(next)
+                frontier.append((priority, next))
+
+                come_from[state_to_tuple(next)] = state_to_tuple(current[-1])
+        end = timeit.default_timer()
+
+        if (end - start) > 10:
+            break
+
+    end = timeit.default_timer()
+    during_time = end - start
+    node_expanded = len(cost_so_far)
+    return state, node_expanded, during_time, result
+
+if __name__ == "__main__":
+    filename = 'heavy queens board.csv'
+    q = Queen()
+    state, node_expanded, during_time, result = a_star(q, filename)
+    print('start states: ')
+    print(state)
+    print('end state: ')
+    print(result)
+    print(during_time)
+    print(node_expanded)
+
+
