a75eb2b53d5dae2bbe703e1c2abfc2bca1d648e0
Hao <44931709+yinhao0424@users.noreply.github.com>
2020-02-24 20:52:48 -0500

    Add files via upload

---
+++ part1 submission/nq_astar.py
@@ -0,0 +1,169 @@
+from NQueens import Queen
+import timeit
+import numpy as np
+
+class a_star(Queen):
+    def __init__(self,filename,heuristic):
+        Queen.__init__(self,filename)
+        self.input_board()
+
+        self.heuristic = heuristic
+        self.frontier = []
+        self.come_from = {}
+        self.cost_so_far = {}
+
+        self.start_time = timeit.default_timer()
+        self.during_time = 0
+
+        self.result = None
+        self.sequential_moves = []
+        self.node_expand = 0
+
+        self.total_cost = 0
+        self.effective_branching_factor = 0
+
+
+    def state_to_tuple(self,state):
+        return tuple(np.nonzero(state.T)[1])
+
+
+    def a_star(self):
+
+        start = self.state_to_tuple(self.start_state)
+        self.frontier.append((0, self.start_state))
+        self.cost_so_far[start] = 0
+
+
+        if self.heuristic == 'H1':
+            while self.frontier:
+                #         sorted(graded, key=lambda x: x[0])
+                # small to large
+                self.frontier.sort(key=lambda x: x[0])
+                current = self.frontier.pop(0)
+
+                if self.heuristic_1(current[-1]) == 0:
+                    self.result = current[-1]
+                    break
+                for next in self.near_state(current[-1]):
+                    new_cost = self.cost_so_far[self.state_to_tuple((current[-1]))] + self.compute_cost(current[-1], next)
+
+                    if self.state_to_tuple(next) not in self.cost_so_far or new_cost < self.cost_so_far[self.state_to_tuple(next)]:
+                        self.cost_so_far[self.state_to_tuple(next)] = new_cost
+
+                        priority = new_cost + self.heuristic_1(next)
+                        self.frontier.append((priority, next))
+
+                        self.come_from[self.state_to_tuple(next)] = self.state_to_tuple(current[-1])
+
+
+                end = timeit.default_timer()
+
+                if (end - self.start_time) > 10:
+                    break
+
+            i = 0
+            self.sequential_moves.append(self.state_to_tuple(self.result))
+            while True:
+                if self.sequential_moves[i] not in self.come_from:
+                    break
+                back = self.come_from[self.sequential_moves[i]]
+                i += 1
+                self.sequential_moves.append(back)
+
+            self.node_expand = len(self.cost_so_far)
+            self.effective_branching_factor = self.node_expand**(1/len(self.sequential_moves))
+            self.total_cost = self.cost_so_far[self.state_to_tuple(self.result)]
+
+        elif self.heuristic == 'H2':
+            while self.frontier:
+                #         sorted(graded, key=lambda x: x[0])
+                # small to large
+                self.frontier.sort(key=lambda x: x[0])
+                current = self.frontier.pop(0)
+
+                if self.heuristic_2(current[-1]) == 0:
+                    self.result = current[-1]
+                    break
+                for next in self.near_state(current[-1]):
+                    new_cost = self.cost_so_far[self.state_to_tuple((current[-1]))] + self.compute_cost(current[-1], next)
+
+                    if self.state_to_tuple(next) not in self.cost_so_far or new_cost < self.cost_so_far[self.state_to_tuple(next)]:
+                        self.cost_so_far[self.state_to_tuple(next)] = new_cost
+
+                        priority = new_cost + self.heuristic_2(next)
+                        self.frontier.append((priority, next))
+
+                        self.come_from[self.state_to_tuple(next)] = self.state_to_tuple(current[-1])
+
+
+                end = timeit.default_timer()
+
+                if (end - self.start_time) > 10:
+                    break
+
+            i = 0
+            self.sequential_moves.append(self.state_to_tuple(self.result))
+            while True:
+                if self.sequential_moves[i] not in self.come_from:
+                    break
+                back = self.come_from[self.sequential_moves[i]]
+                i += 1
+                self.sequential_moves.append(back)
+
+            self.node_expand = len(self.cost_so_far)
+            self.effective_branching_factor = self.node_expand**(1/len(self.sequential_moves))
+            self.total_cost = self.cost_so_far[self.state_to_tuple(self.result)]
+
+
+        end = timeit.default_timer()
+        self.during_time = end - self.start_time
+
+        # return state, node_expanded, during_time, result
+
+if __name__ == "__main__":
+    """
+    output：
+    1. start state
+    2. the number of nodes expanded
+    3. time to solve the puzzle
+    4. the effective branching factor
+    5. the cost to solve the puzzle
+    6. the sequence of moves
+
+    """
+    filename = 'heavy queens board.csv'
+    astar = a_star(filename,'H2')
+    astar.a_star()
+
+    print('Start state:')
+    print(astar.start_state)
+
+    print('The number of nodes expanded:')
+    print(astar.node_expand)
+
+    print("Time to solve the puzzle: ")
+    print(astar.during_time)
+
+    print('The effective branching factor:' )
+    print(astar.effective_branching_factor)
+
+    print("The total cost: ")
+    print(astar.total_cost)
+
+    print('The sequence of moves:')
+    print(astar.sequential_moves[::-1])
+
+    print('Final state')
+    print(astar.result)
+
+
+    # q = Queen()
+    # state, node_expanded, during_time, result = a_star(q, filename,'H1')
+    # print('start states: ')
+    # print(state)
+    # print('end state: ')
+    # print(result)
+    # print(during_time)
+    # print(node_expanded)
+
+
---
+++ part1 submission/nq_hillclimbing.py
@@ -0,0 +1,182 @@
+from NQueens import Queen
+import timeit
+import random
+import copy
+import numpy as np
+
+class hill_climbing(Queen):
+    def __init__(self,filename,heuristic):
+        Queen.__init__(self,filename)
+        self.input_board()
+
+        self.heuristic = heuristic
+        self.sideway_walk = 5
+        self.sequential_moves = [self.state_to_board(self.start_state)]
+        self.sequential_moves_restart = []
+
+        self.cost = 0
+        self.total_cost = 0
+
+        self.expanded_node=0
+
+        self.current = copy.deepcopy(self.start_state)
+        self.restart = 0
+
+        self.start_time = timeit.default_timer()
+        self.during_time = 0
+
+    def hill_climbing(self):
+
+        if self.heuristic =='H1':
+            while self.heuristic_1(self.current) != 0 and self.sideway_walk > 0:
+                neighbours = self.near_state(self.current)
+                if not neighbours:
+                    break
+                # find neighbour with min H1
+                neighbour = min(neighbours, key=lambda state: self.heuristic_1(state))
+
+                if self.heuristic_1(neighbour) == self.heuristic_1(self.current) and self.sideway_walk > 0:
+                    # find neighbour with the same value with current
+                    neighbour_side = [v for i, v in enumerate(neighbours) if
+                                      self.heuristic_1(v) == self.heuristic_1(self.current)]
+                    neighbour = random.choice(neighbour_side)
+                    self.sideway_walk -= 1
+
+                if self.heuristic_1(neighbour) > self.heuristic_1(self.current):
+                    break
+
+                self.cost += self.compute_cost(neighbour, self.current)
+                self.current = neighbour
+                self.sequential_moves.append(self.state_to_board(self.current))
+
+        elif self.heuristic =='H2':
+            while self.heuristic_2(self.current) != 0 and self.sideway_walk > 0:
+                neighbours = self.near_state(self.current)
+                if not neighbours:
+                    break
+                # find neighbour with min H1
+                neighbour = min(neighbours, key=lambda state: self.heuristic_2(state))
+
+                if self.heuristic_2(neighbour) == self.heuristic_2(self.current) and self.sideway_walk > 0:
+                    # find neighbour with the same value with current
+                    neighbour_side = [v for i, v in enumerate(neighbours) if
+                                      self.heuristic_2(v) == self.heuristic_2(self.current)]
+                    neighbour = random.choice(neighbour_side)
+                    self.sideway_walk -= 1
+
+                if self.heuristic_2(neighbour) > self.heuristic_2(self.current):
+                    break
+
+                self.cost += self.compute_cost(neighbour, self.current)
+                self.current = neighbour
+                self.sequential_moves.append(self.state_to_board(self.current))
+
+
+    # HC com random restart
+    def restarts(self):
+
+        if self.heuristic == 'H1':
+            self.hill_climbing()
+            self.sequential_moves_restart.extend(self.sequential_moves)
+            self.total_cost = self.cost
+
+            while self.heuristic_1(self.current) != 0:
+                self.current = copy.deepcopy(self.start_state)
+                self.sideway_walk = 5
+                self.hill_climbing()
+                self.restart += 1
+                self.total_cost += self.cost + self.compute_cost(self.start_state,self.current)
+                self.sequential_moves_restart.extend(self.sequential_moves)
+                end = timeit.default_timer()
+                if (end - self.start_time) > 10:
+                    break
+
+            self.during_time = timeit.default_timer() - self.start_time
+            self.expanded_node = len(self.sequential_moves_restart)
+
+        elif self.heuristic == 'H2':
+            self.hill_climbing()
+            self.sequential_moves_restart.extend(self.sequential_moves)
+            self.total_cost = self.cost
+
+            while self.heuristic_2(self.current) != 0:
+                self.current = copy.deepcopy(self.start_state)
+                self.sideway_walk = 5
+                self.hill_climbing()
+                self.restart += 1
+                self.total_cost += self.cost + self.compute_cost(self.start_state,self.current)
+                self.sequential_moves_restart.extend(self.sequential_moves)
+                end = timeit.default_timer()
+                if (end - self.start_time) > 10:
+                    break
+
+            self.during_time = timeit.default_timer() - self.start_time
+            self.expanded_node = len(self.sequential_moves_restart)
+
+
+if __name__ == "__main__":
+    """
+    output：
+    1. start state
+    2. the number of nodes expanded
+    3. time to solve the puzzle
+    4. the effective branching factor
+    5. the cost to solve the puzzle
+    6. the sequence of moves
+    
+    """
+    # Test for restart
+    hc = hill_climbing('heavy queens board.csv', "H2")
+    hc.restarts()
+    print('Start state:')
+    print(hc.start_state)
+    print('The number of nodes expanded:')
+    print(hc.expanded_node)
+    print("Time to solve the puzzle: ")
+    print(hc.during_time)
+    print('The effective branching factor:' + str(1))
+    print("The total cost: ")
+    print(hc.total_cost)
+    print('The sequence of moves with restart:')
+    print(hc.sequential_moves_restart)
+
+    print('The number of restarts')
+    print(hc.restart)
+
+    print('Final state')
+    print(hc.current)
+
+
+    # Test for hill climbing h2
+    # hc = hill_climbing('heavy queens board.csv',"H2")
+    # hc.hill_climbing()
+    # print('Start state:')
+    # print(hc.start_state)
+    # print('The number of nodes expanded:')
+    # print(len(hc.sequential_moves))
+    # print("The cost: ")
+    # print(hc.cost)
+    # print('The sequence of moves:')
+    # print(hc.sequential_moves)
+    #
+    # print('Final state')
+    # print(hc.current)
+    # print(hc.sideway_walk)
+
+
+    #  Test for hill climbing h1
+    # hc = hill_climbing('heavy queens board.csv',"H1")
+    # hc.hill_climbing()
+    # print('Start state:')
+    # print(hc.start_state)
+    # print('The number of nodes expanded:')
+    # print(len(hc.sequential_moves))
+    # print("The cost: ")
+    # print(hc.cost)
+    # print('The sequence of moves:')
+    # print(hc.sequential_moves)
+    #
+    # print('Final state')
+    # print(hc.current)
+
+
